58b96bc72dd79b37aebfad4a64e808da810f0f7f
camperD <damyn.chipman@gmail.com>
2019-01-21 15:36:06 -0700

    Saving progress, VSB and SVPM coupling. TODO: Determine scaling coef.

--- verifications/VSB_Verification1.jl
+++ verifications/VSB_Verification1.jl
@@ -1,11 +1,17 @@
 # ===== PM2D Verifications: Pressure Distribution =====
-println("HERE")
+
+println("=== BEGINNING Verifications1.jl ===")
+
 # == Imports ==
 using VSB
 using SciTools
-println("HERE")
+using LinearAlgebra
+using Plots
+pyplot()
+using LaTeXStrings
+
 # == Set up geometry and grids ==
-N = 200                             # Number of points
+N = 100                             # Number of points
 NPAN = N - 1                        # Number of panels
 del_theta = (2*pi)/(N-1)            # Spacing in theta
 R = 1.0                             # Radius
@@ -15,13 +21,15 @@ theta = zeros(NPAN-1)               # Angle from positive X-axis
 CP_analytical = zeros(NPAN-1)       # Analytical CP
 CP_PM2D = zeros(NPAN-1)             # PM2D (Semi-Analytical) CP
 CP_SVPM = zeros(NPAN-1)             # SVPM CP
-for i=1:NPAN-1
+for i=1:NPAN
     X[i] = R*cos((i-1)*del_theta)
     Y[i] = R*sin((i-1)*del_theta)
+    #println("X[i] = ",X[i],"   Y[i] = ",Y[i])
     if i != N-1
         theta[i] = (i-1)*del_theta
     end
 end
+X[N] = 1.0; Y[N] = 0.0
 
 # == CP: Analytical ==
 for i=1:NPAN-1
@@ -31,12 +39,15 @@ end
 # == CP: VSB w/ Summation of Linear Vortex Lines (Semi-Analytical) ==
 oper_cond = [10.0,0.0]
 panels = SciTools.Body2D(X,Y)
-RHS = ones(NPAN) * 10.0
+RHS = zeros(NPAN-1)
+for i=1:NPAN-1
+    RHS[i] = dot(panels[i].t_hat,oper_cond)
+end
 alpha_coefs = VSB.CalcVortexSheetCoef(panels,RHS)
 
 # Wrapper function for
 function u_gamma(X)
-    U = VSB.CalcVortexSheetVelocity(panels,alpha_coefs,X,sigma=0.2,a=18.5*pi)
+    U = VSB.CalcVortexSheetVelocity(panels,alpha_coefs,X,sigma=1.0,a=10.0)
     return U
 end
 
@@ -62,17 +73,11 @@ prompt = true                           # Whether to prompt the user
 verbose = true                          # Whether to print verbose
 
 # ------------- SIMULATION PARAMETERS -------------------------------------
-sigma = 1.0               # Smoothing radius
+sigma = 1.0                # Smoothing radius
 nu = 1e1                  # Kinematic viscosity
 
-magUinf = 0
+magUinf = 10.0
 Uinf(t) = magUinf*[1, 0, 0]
-nsteps = 150              # Number of time steps
-dt = 0.001                # Time step
-
-nsteps_relax = 1          # Steps in between relaxation
-sgm0 = sigma              # Default core size
-beta_cs = 1.1            # Maximum core size growth
 
 # -------------- PARTICLE FIELD --------------------------------------------
 # Initiates the field
@@ -89,7 +94,7 @@ pfield = ParticleField(nu,
 
 for i=1:NPAN-1
     loc = [X[i], Y[i], 0.0]
-    Gamma = alpha_coefs[i]
+    Gamma = [0.0, 0.0, VSB.CalcVortexSheet(panels,alpha_coefs,loc[1:2])]
     vol = 1.0
     particle = Particle(loc, Gamma, sigma, vol)
     addparticle(pfield, particle)
@@ -98,3 +103,22 @@ end
 for i=1:NPAN-1
     CP_SVPM[i] = 1 - (norm(U(pfield, [X[i],Y[i],0.0]))/(oper_cond[1]))^2
 end
+
+# == Plotting Results ==
+println("=== BEGINNING PLOTTING ===")
+
+plt1 = plot(theta,CP_analytical,line = (:red),
+                                label = "Analytical")
+plot!(theta,CP_PM2D,line = (:blue, :dash),
+                    label = "Semi-Analytical")
+plot!(theta,-CP_SVPM,line = (:green, :dash),
+                     title = "VSB Verification - Pressure Distribution",
+                     xlabel = L"\theta",
+                     xlims = (0.0, 2*pi),
+                     xticks = ([0.0,pi/2,pi,3*pi/2,2*pi],["0",L"\frac{\pi}{2}",L"\pi",L"\frac{3\pi}{2}",L"2\pi"]),
+                     ylabel = L"C_p",
+                     ylims = (-4,2),
+                     yflip = true,
+                     label = "Coupled")
+display(plt1)
+savefig(plt1,"fig1.png")
