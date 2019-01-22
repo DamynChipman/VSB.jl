3d9843ef2bd3e9c8452ccf492b9bba6b72c56f74
camperD <damyn.chipman@gmail.com>
2019-01-21 23:37:29 -0700

    Added structure of VortexDiffusion

--- src/VSB_VortexDiffusion.jl
+++ src/VSB_VortexDiffusion.jl
@@ -10,6 +10,16 @@
 * () : []
 
 """
-function VortexDiffusion()
+function VortexDiffusion(pfield::SVPM.ParticleField,
+                         boundary::Boundary,
+                         dt::Real,
+                         RHS::Array{T,1};
+                         theta_CN::Real=0.5) where {T<:Reals}
+
+    # Helper functions
+
+    # Build matrix
+
+    # Solve system for coefs
     
 end
