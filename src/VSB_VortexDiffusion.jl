008407a17e592de98a9f5295b179f19ad80e2291
camperD <damyn.chipman@gmail.com>
2019-02-05 14:03:03 -0700

    Debugging singularity error

--- src/VSB_VortexDiffusion.jl
+++ src/VSB_VortexDiffusion.jl
@@ -40,7 +40,7 @@ function VortexDiffusion(pfield::SVPM.ParticleField,
                 R = r(Xi,Xj)
                 RHAT = rHat(Xi,Xj)
                 A[i,j] = dot(RBF_gauss(R,deriv=1).*RHAT, boundary.nHats[i])
-                println("I != J ... A[",i,", ",j,"] = ",A[i,j])
+                println(" Xi = ",Xi," ... Xj = ",Xj," ... A[",i,", ",j,"] = ",A[i,j])
             else
                 R = 0.0
                 RHAT = [0.0, 0.0, 0.0]
@@ -59,7 +59,7 @@ function VortexDiffusion(pfield::SVPM.ParticleField,
                 Xj = pfield.particles[j].X
                 R = r(Xi,Xj)
                 A[i,j] = RBF_gauss(R,deriv=0) - CONST1 * RBF_gauss(R,deriv=2)
-                println("I != J ... A[",i,", ",j,"] = ",A[i,j])
+                println(" Xi = ",Xi," ... Xj = ",Xj," ... A[",i,", ",j,"] = ",A[i,j])
             else
                 R = 0.0
                 A[i,j] = RBF_gauss(R,deriv=0) - CONST1 * RBF_gauss(R,deriv=2)
