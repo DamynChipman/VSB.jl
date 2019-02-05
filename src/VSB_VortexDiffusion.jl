2b298383fa333d8c8121b41c87d20989e710e4da
camperD <damyn.chipman@gmail.com>
2019-02-05 13:35:46 -0700

    Correcting for divide by 0 resulting in NaN

--- src/VSB_VortexDiffusion.jl
+++ src/VSB_VortexDiffusion.jl
@@ -34,11 +34,17 @@ function VortexDiffusion(pfield::SVPM.ParticleField,
     # NxM Matrix (upper half)
     for i = 1:N_BODY
         for j = 1:N_FIELD
-            Xi = pfield.particles[i].X
-            Xj = pfield.particles[j].X
-            R = r(Xi,Xj)
-            RHAT = rHat(Xi,Xj)
-            A[i,j] = dot(RBF_gauss(R,deriv=1).*RHAT, boundary.nHats[i])
+            if i != j
+                Xi = pfield.particles[i].X
+                Xj = pfield.particles[j].X
+                R = r(Xi,Xj)
+                RHAT = rHat(Xi,Xj)
+                A[i,j] = dot(RBF_gauss(R,deriv=1).*RHAT, boundary.nHats[i])
+            else
+                R = 0.0
+                RHAT = [0.0, 0.0]
+                A[i,j] = dot(RBF_gauss(R,deriv=1).*RHAT, boundary.nHats[i])
+            end
             println("A[",i,", ",j,"] = ",A[i,j])
         end
     end
@@ -46,10 +52,15 @@ function VortexDiffusion(pfield::SVPM.ParticleField,
     # (M-N+1)xM matrix (lower half)
     for i = (N_BODY+1):N_FIELD
         for j = 1:N_FIELD
-            Xi = pfield.particles[i].X
-            Xj = pfield.particles[j].X
-            R = r(Xi,Xj)
-            A[i,j] = RBF_gauss(R,deriv=0) - CONST1 * RBF_gauss(R,deriv=2)
+            if i != j
+                Xi = pfield.particles[i].X
+                Xj = pfield.particles[j].X
+                R = r(Xi,Xj)
+                A[i,j] = RBF_gauss(R,deriv=0) - CONST1 * RBF_gauss(R,deriv=2)
+            else
+                R = 0.0
+                A[i,j] = RBF_gauss(R,deriv=0) - CONST1 * RBF_gauss(R,deriv=2)
+            end
             println("A[",i,", ",j,"] = ",A[i,j])
         end
     end
