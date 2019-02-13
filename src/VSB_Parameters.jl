21cb62bc10c7b74abe0fa5fb46c9100edcdee8e3
camperD <damyn.chipman@gmail.com>
2019-02-12 21:56:49 -0700

    Adding Parameters struct

--- src/VSB_Parameters.jl
+++ src/VSB_Parameters.jl
@@ -7,10 +7,20 @@ mutable struct Parameters
     U_field::Union{Nothing, Function}
     alphas::Array{T} where {T<:Real}
     etas::Array{T} where {T<:Real}
+    verbose::Bool
 
-    function Parameters(boundary, U_field)
+    function Parameters(boundary, U_field; verbose = false)
 
+        # === Calculate etas ===
+        if verbose
+            println("   CALCULATING ETAS IN PARAMETERS...")
+        end
         etas = CalcRhoCoefs(boundary)
+
+        # === Calculate alphas ===
+        if verbose
+            println("   CALCULATING ALPHAS IN PARAMETERS...")
+        end
         alphas = CalcVSCoefs(boundary, etas, U_slip = U_field)
 
         new(boundary, U_field, alphas, etas)
