20507a8e9082f5899a9712f337473df728893047
camperD <damyn.chipman@gmail.com>
2019-02-18 18:17:53 -0700

    Different method of VS diffusion

--- src/VSB_VortexDiffusion.jl
+++ src/VSB_VortexDiffusion.jl
@@ -94,6 +94,56 @@ end
 """
 
 """
+function CalcVSDiffusionCoefs(boundary::Boundary,
+                              alphas::Array{T},
+                              dt::Real,
+                              nu::Real)
+
+    # === Extract geometry from boundary ===
+    X = [[point[1], point[2], 0.0] for point in boundary.bodyPTS]
+    n_hats = [[n_hat[1], n_hat[2]] for n_hat in boundary.nHats]
+    t_hats = [[t_hat[1], t_hat[2]] for t_hat in boundary.tHats]
+    NPTS = boundary.NPTS_BODY
+
+    # === Constants ===
+    CONST1 = -1 / (nu * dt)
+
+    # === Build matrix ===
+    A = zeros(NPTS, NPTS)
+    for k in 1:NPTS
+        for i in 1:NPTS
+            if k != i
+                R = X[k] - X[i]
+                r = norm(R)
+                r_hat = R ./ r
+                n_hat = n_hats[k]
+                A[k,i] = dot(RBF_gauss(r, deriv = 1) .* r_hat, n_hat)
+            else
+                R = [0, 0, 0]
+                r = norm(R)
+                r_hat = [0, 0, 0]
+                n_hat = n_hats[k]
+                A[k,i] = dot(RBF_gauss(r, deriv = 1) .* r_hat, n_hat)
+            end
+        end
+    end
+
+    # === Build RHS ===
+    b = zeros(NPTS)
+    for k in 1:NPTS
+        b[k] = CalcVS(boundary, alphas, X[k]) * CONST1
+    end
+
+    # === Solve linear system ===
+    betas = A\b
+    return betas
+
+end
+
+
+"""
+
+"""
 function CalcDiffusion(boundary::Boundary,
                        beta::Array{T},
                        X_eval::Array{T}) where {T<:Real}
