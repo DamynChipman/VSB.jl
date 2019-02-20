a1a5f1a08026f4ad6e640b51ba303ac23c0a005f
camperD <damyn.chipman@gmail.com>
2019-02-20 12:53:23 -0700

    Debugging basis with VPM basis

--- src/VSB_RBF.jl
+++ src/VSB_RBF.jl
@@ -20,7 +20,10 @@ function RBF_gauss(R::Float64;
                    A=1.0,sigma=0.2,deriv=0)
 
     if deriv == 0
-        res = SVPM.zeta_gauserf(R/sigma)
+        const const1 = 1/(2*pi)^(3/2)
+        res = const1*exp(-R^2/2)
+
+        #res = SVPM.zeta_gauserf(R/sigma)
         #res = A*exp(-(R^2)/(2*sigma^2))
     elseif deriv == 1
         res = -((A*R)/(sigma^2))*exp(-(R^2)/(2*sigma^2))
